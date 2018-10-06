a7c9facf84f697573e40cc983c25db33c1a38da6
YUKI <>
2018-10-07 04:29:01 +0900

    除外設定

---
+++ config/config.js
@@ -0,0 +1,13 @@
+exports.slack = {
+  host: 'hooks.slack.com',
+  path: 'WebhookのURL',
+  cannel: '投稿するチャンネル',
+};
+
+exports.spreadsheets = {
+  host: {
+    pc: 'PCサイトのドメイン',
+    sp: 'SPサイトのドメイン',
+  },
+  release: `スプレッドシートID`,
+}
